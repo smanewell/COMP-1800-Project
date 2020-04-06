473cb833cbb51565481af2f2d5f188a9b26ec8b8
smanewell <snewell5@my.bcit.ca>
2020-04-06 16:01:42 -0700

    edits to forecaster and landing page

--- forecaster.js
+++ forecaster.js
@@ -1,19 +1,30 @@
+const fetch = require('node-fetch')
 
-function getForecast() {
+async function getForecast() {
 
     if (navigator.geolocation) {
         let coordinates = navigator.geolocation.getCurrentPosition(getCoordinates);
       }
-
     else {
         alert("Geolocation is not supported by this browser.");
       }
 }
 
-function getCoordinates(position) {
+
+async function getCoordinates(position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
-    alert('Latitude: ' + latitude + ', Longitude: ' + longitude)
+    //alert('Latitude: ' + latitude + ', Longitude: ' + longitude)
+    apiURL = "https://api.darksky.net/forecast/c091c8ff8c07a5b4ffebf5621ce1310d/" + latitude + ',' + longitude;
+    const darkskyResponse = await fetch(apiURL);
+    const forecast = await darkskyResponse.json();
+}
+
+async function displayWeeklyForecast(divisionID) {
+  await getForecast();
+  displayElement = document.getElementById(divisionID);
+  console.log(forecast);
+
 }
 
 // https://api.darksky.net/forecast/c091c8ff8c07a5b4ffebf5621ce1310d/latitude,longitude
