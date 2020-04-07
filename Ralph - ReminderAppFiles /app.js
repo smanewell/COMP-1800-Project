b4b2d57658aa23d46b0ad3574a3c374e7fd09095
ralphkilian <rkilian1@my.bcit.ca>
2020-04-07 08:04:37 -0700

    some integration of my app files and armaans reminderapp example

--- Ralph - ReminderAppFiles /app.js
+++ Ralph - ReminderAppFiles /app.js
@@ -21,25 +21,25 @@ function getCoordinates(position) {
 
 // add subtask
 
-// Create a "close" button and append it to each list item
-let myNodelist = document.getElementsByTagName("LI");
-for (let i = 0; i < myNodelist.length; i++) {
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+// // Create a "close" button and append it to each list item
+// let myNodelist = document.getElementsByTagName("LI");
+// for (let i = 0; i < myNodelist.length; i++) {
+//   let span = document.createElement("SPAN");
+//   let txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   myNodelist[i].appendChild(span);
+// }
 
-// Click on a close button to hide the current list item
-let close = document.getElementsByClassName("close");
+// // Click on a close button to hide the current list item
+// let close = document.getElementsByClassName("close");
 
-for (let i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    let div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+// for (let i = 0; i < close.length; i++) {
+//   close[i].onclick = function() {
+//     let div = this.parentElement;
+//     div.style.display = "none";
+//   }
+// }
 
 // Add a "checked" symbol when clicking on a list item
 let list = document.querySelector('ul.ul2');
---
+++ reminderapp/database.js
@@ -0,0 +1,12 @@
+
+let Database = {
+    cindy: {
+        reminders: [{title: "Example Reminder", datetime: "today", tasks: ["task 1", "task 2"]},
+        {title: "Reminder 2", datetime: "today", tasks: ["task 1", "task 2"]}]
+    },
+    alex: {
+        reminders: []
+    } 
+}
+
+module.exports = Database;
